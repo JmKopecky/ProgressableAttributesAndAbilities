fd2dccc7287f532afe2ce5815baa0750f3062aaa
Joseph Kopecky <josephkopecky@jmkopecky.tech>
2023-10-20 09:30:15 -0500

    Added an example implementation of gaining xp from playing, and listed other options for xp gain.

--- src/main/java/dev/prognitio/pa3/ModEvents.java
+++ src/main/java/dev/prognitio/pa3/ModEvents.java
@@ -5,11 +5,33 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid=pa3.MODID)
 public class ModEvents {
+
+
+    //example for adding xp for breaking blocks
+    @SubscribeEvent
+    public static void onBlockBreak(BlockEvent.BreakEvent event) {
+        if (event.getPlayer() != null) {
+           event.getPlayer().getCapability(AttributesProvider.ATTRIBUTES).ifPresent((attributes -> {
+                attributes.addXP(5.0);
+            }));
+        }
+    }
+    /*
+    other ideas:
+    AdvancementEvent
+    EntityPlaceEvent
+    ItemCraftedEvent
+    ItemSmeltedEvent
+    LivingDeathEvent
+     */
+
+
 
     @SubscribeEvent
     public static void onAttachCapabilitiesPlayer(AttachCapabilitiesEvent<Player> event) {
